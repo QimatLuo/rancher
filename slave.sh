f6eaf3603cec364cf70817e85d4b1521877b4ab6
Qimat Luo <qimatluo@westpharma.com>
2025-06-01 23:43:03 +0800

    Add slave

---
+++ slave.sh
@@ -0,0 +1,4 @@
+export K3S_TOKEN="K10fc27a8c9d97cc8cccaa9258271b82795c33304cf6063daf5a38154ab633a88ad::server:e6c936b69c6e4412168493f32d00e124"
+export K3S_URL="https://192.168.1.116:6443"
+export K3S_NODE_NAME="eb-o"
+curl -sfL https://get.k3s.io | sh -s -
